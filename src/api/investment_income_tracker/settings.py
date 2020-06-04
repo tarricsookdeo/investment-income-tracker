333ec2f9e9cb4d70473aad170ed2521d1a52fff2
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-06-03 20:48:39 -0400

    Configure 3rd party apps to Django settings

--- src/api/investment_income_tracker/settings.py
+++ src/api/investment_income_tracker/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Corsheaders settings
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'  # React development server
+]
