0b64bc6f6d6ec7c6c499727fe49361ccabd0901c
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-06-03 21:11:45 -0400

    Initial Positions model

--- src/api/positions/models.py
+++ src/api/positions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    ticker = models.CharField(max_length=5)
+    shares = models.DecimalField(max_digits=10, decimal_places=6)
+    avg_price_per_share = models.DecimalField(max_digits=9, decimal_places=3)
+
+    def __str__(self):
+        return self.ticker
